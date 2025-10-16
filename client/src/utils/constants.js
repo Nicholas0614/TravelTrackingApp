e3127a5596ea210ff3da14e7be07b5335e78ce10
Nicholas <nicholaskuokjinshumg@gmail.com>
2025-10-16 08:39:37 +0800

    update

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,3 +1,3 @@
 export const API_URL = import.meta.env.ENV
-  ? "http://localhost:1122/api"
-  : "https://b15-nicholas.mak3r.dev/api";
+  ? "http://localhost:1122/api/"
+  : "https://b15-nicholas.mak3r.dev/api/";
